cc59634a9fae70e085b1fcbeec0641e0acc58409
Christopher N. Katoyi Kaba <me@christopher2k.dev>
2026-04-05 17:53:23 -0400

    feat: initial commit

---
+++ devenv.nix
@@ -0,0 +1,13 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  packages = [ pkgs.git ];
+  languages.go.enable = true;
+  languages.go.version = "1.26.1";
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/christopher2k/stream-chat-cli
+
+go 1.26.1
